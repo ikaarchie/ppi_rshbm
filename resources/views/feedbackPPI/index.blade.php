d587756789b095eeddcaad97c04f9b4f90652853
ikaarchie <ikaarchie8@gmail.com>
2023-06-27 21:45:42 +0700

    bug feedback

--- resources/views/feedbackPPI/index.blade.php
+++ resources/views/feedbackPPI/index.blade.php
@@ -80,7 +80,7 @@
                 <tr>
                     <td colspan="5" class="text-center"><b>Tidak ada data</b></td>
                 </tr>
-                {{ $sisi->dokumen }}
+                {{-- {{ $isi->dokumen }} --}}
                 @endforelse
             </tbody>
         </table>
